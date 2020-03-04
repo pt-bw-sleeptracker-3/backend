f971704e5ae56ab530781da87915db8181c4ebd4
Hector Valdez <hvaldez180@gmail.com>
2020-03-03 19:42:43 -0500

    debugging config

--- data/db-config.js
+++ data/db-config.js
@@ -1,13 +1,13 @@
-// const knex = require('knex');
-
-// const knexfile = require('../knexfile.js');
-
-// const environment = process.env.NODE_ENV || 'development';
-
-// module.exports = knex(knexfile[environment]);
-
 const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
+const knexfile = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const environment = process.env.DB_ENV || 'development';
+
+module.exports = knex(knexfile[environment]);
+
+// const knex = require('knex');
+
+// const knexConfig = require('../knexfile.js');
+
+// module.exports = knex(knexConfig.development);
